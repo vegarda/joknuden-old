6ca304d3f2dc669438ea42bcf869fcaa88cf44c4
Vegard Andersen <ferska.vara@gmail.com>
2014-07-04 21:37:50 +0200

    initial commit

---
+++ bin/weeutil/__init__.py
@@ -0,0 +1,9 @@
+#
+#    Copyright (c) 2009 Tom Keffer <tkeffer@gmail.com>
+#
+#    See the file LICENSE.txt for your full rights.
+#
+#    $Revision: 1459 $
+#    $Author: mwall $
+#    $Date: 2013-10-08 17:44:50 -0700 (Tue, 08 Oct 2013) $
+#
