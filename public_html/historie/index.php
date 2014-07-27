dc8caa5918e302753b2dbb45ac456918fa4fcd29
Vegard Andersen <ferska.vara@gmail.com>
2014-07-27 13:57:31 +0200

    css fix

--- public_html/historie/index.php
+++ public_html/historie/index.php
@@ -5,10 +5,13 @@
 		<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 		<title>Joknuden</title>		
 		<link rel="stylesheet" type="text/css" href="../stylesheet.css"/>
+		<link rel="stylesheet" type="text/css" href="../navigation.css"/>
 		<link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,700,600,300" rel="stylesheet" type="text/css">
 		<script type="text/javascript" src="../jquery-2.1.1.min.js"></script>
 		<script type="text/javascript" src="../analytics.js"></script>
-		<script type="text/javascript" src="../navigation.js"></script>';
+		<script type="text/javascript" src="../navigation.js"></script>
+		
+		';
 echo '
 	</head>
 	<body>';
