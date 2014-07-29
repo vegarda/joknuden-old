f40e78f131244ea78a8a5374b7fa52ed8d2023e3
Vegard Andersen <ferska.vara@gmail.com>
2014-07-29 13:42:55 +0200

    fix

--- public_html/mesowx/index.php
+++ public_html/mesowx/index.php
@@ -6,7 +6,7 @@
 		<title>Joknuden</title>
 		<link href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,400,700,600,300" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" type="text/css" href="style/mesowx.css"/>
-		<link rel="stylesheet" type="text/css" href="../navigation.css"/>
+		<link rel="stylesheet" type="text/css" href="../css/navigation.css"/>
 		<script type="text/javascript" src="../jquery-2.1.1.min.js"></script>
 		<script type="text/javascript" src="../analytics.js"></script>
 		<script type="text/javascript" src="../navigation.js"></script>
