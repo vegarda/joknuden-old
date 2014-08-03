261a702dd29297b66e443121d782a9372a441eb2
Vegard Andersen <ferska.vara@gmail.com>
2014-08-03 21:00:50 +0200

    Remove old

--- public_html/navigation.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-        //console.log(window.location.pathname)
-		$('li.active').removeClass('active');
-		console.log(window.location.pathname);
-		$("a[href=\""+window.location.pathname+"\"]").parent().addClass('active');
-  });
