f837ba4ec51742f5fb43a683e88a88253ea80302
Vegard Andersen <ferska.vara@gmail.com>
2014-08-08 00:13:32 +0200

    Catch curl error4

--- data/proxy.php
+++ data/proxy.php
@@ -13,11 +13,29 @@
             'Accept-Language: nb-no,nb;q=0.9,no-no;q=0.8,no;q=0.6,nn-no;q=0.5,nn;q=0.4,en-us;q=0.3,en;q=0.1',
             'Accept-Encoding: gzip,deflate,sdch'
         ));
-        curl_exec($ch);
-        if(curl_errno($ch))
-        {
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Mozilla/4.0 (compatible; MSIE 5.01; Windows NT 5.0)');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+
+        /* try to follow redirects */
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+        /* timeout after the specified number of seconds. assuming that this script runs
+        on a server, 20 seconds should be plenty of time to verify a valid URL.  */
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 10);
+        $response = curl_exec($ch);
+        
+        if(curl_errno($ch)){
             echo 'Curl error: ' . curl_error($ch);
         }
-        curl_close($ch);    
+        
+        if($response === false){
+            echo 'Curl error: ' . curl_error($ch);
+        }
+        
+        curl_close($ch);
+        
+        echo $response;
+        
     };
 ?>
