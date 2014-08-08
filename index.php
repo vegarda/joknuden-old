5da7d3baf27dd3bf471584aece6d3204c47b907c
Vegard Andersen <ferska.vara@gmail.com>
2014-08-08 03:24:54 +0200

    Add RADAR + fancybox

--- index.php
+++ index.php
@@ -99,11 +99,24 @@ echo '
 
     
 	<script type="text/javascript" src="/js/warning.js"></script>
+	<script type="text/javascript" src="/js/sun.js"></script>
     <script type="text/javascript" src="/highcharts/js/highstock.js"></script>
     <script type="text/javascript" src="/highcharts/js/highcharts-more.js"></script>
     <script type="text/javascript" src="/highcharts/charts.js"></script>
     <script type="text/javascript" src="/highcharts/chartbuilder.js"></script>
     <script type="text/javascript" src="/highcharts/forecast.js"></script>
+    
+    <!-- Add fancyBox -->
+    <link rel="stylesheet" href="/fancybox/source/jquery.fancybox.css?v=2.1.5" type="text/css" media="screen" />
+    <script type="text/javascript" src="/fancybox/source/jquery.fancybox.pack.js?v=2.1.5"></script>
+
+    <!-- Optionally add helpers - button, thumbnail and/or media -->
+    <link rel="stylesheet" href="/fancybox/source/helpers/jquery.fancybox-buttons.css?v=1.0.5" type="text/css" media="screen" />
+    <script type="text/javascript" src="/fancybox/source/helpers/jquery.fancybox-buttons.js?v=1.0.5"></script>
+    <script type="text/javascript" src="/fancybox/source/helpers/jquery.fancybox-media.js?v=1.0.6"></script>
+
+    <link rel="stylesheet" href="/fancybox/source/helpers/jquery.fancybox-thumbs.css?v=1.0.7" type="text/css" media="screen" />
+    <script type="text/javascript" src="/fancybox/source/helpers/jquery.fancybox-thumbs.js?v=1.0.7"></script>
     
     <script type="text/javascript">
         $( document ).ready(function() {
@@ -131,6 +144,8 @@ echo '
             
             var mesowxConsole = new mesowx.MesoWxConsole(mesowx.Config.consoleOptions);
             var windCompass = new mesowx.MesoWxWindCompass(mesowx.Config.windCompassOptions);
+            
+            $(".fancybox").fancybox();
         });
     </script>
 
@@ -163,6 +178,7 @@ echo '
             <li><a id="month" href="/month/1/">Month</a></li>
             <li><a id="6-month" href="/month/6/">6 Month</a></li>
             <li><a id="year" href="/year/1/">Year</a></li>
+            <li><a id="radar" class="fancybox" rel="group" href="http://api.yr.no/weatherapi/radar/1.4/?radarsite=southwest_norway;type=reflectivity;content=image;size=large#.jpg" alt="">RADAR</a></li>
           </ul>
         </div><!--/.nav-collapse -->
       </div>
