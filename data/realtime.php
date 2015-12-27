758244c528f1cbb146a0c01e3b72c792b2dda53a
Vegard Andersen <ferska.vara@gmail.com>
2015-12-27 23:23:42 +0100

    change realtime queries+

---
+++ data/realtime.php
@@ -0,0 +1,18 @@
+<?php
+
+include('config.php');
+	
+$joknuden = mysqli_connect($host, $user, $pass) or die(mysql_error()); 
+
+$realtime_query = mysqli_query($joknuden, 
+"SELECT *
+FROM weewx.raw 
+ORDER BY dateTime DESC
+LIMIT 1");
+
+$realtime = mysqli_fetch_assoc($realtime_query);
+
+echo json_encode($realtime);
+
+
+?>
