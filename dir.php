5580ca11748e88356fa441efaca19583632094e5
Vegard Andersen <ferska.vara@gmail.com>
2015-12-30 12:25:53 +0100

    Fix webcam image links

--- dir.php
+++ dir.php
@@ -1,13 +1,14 @@
 <?php	
 
 $day    = date("Y-m-d");
-$dir    = '/var/www/html/timelapse/'.$day;
+//$dir    = '/var/www/html/timelapse/'.$day;
+$dir    = $_SERVER['DOCUMENT_ROOT'].'/timelapse/'.$day;
 $images = scandir($dir, 1);
 $images = array_slice($images,1);
 
 foreach ($images as $image){
 	echo '
-			<a id="webcam" class="fancybox hidden" rel="webcam" title="View from Joknuden" href="'.$REQUEST_URI.'/'.$day.'/'.$image.'.jpg" alt="">$image</a>';
+			<a id="webcam" class="fancybox hidden" rel="webcam" title="View from Joknuden" href="'.$REQUEST_URI.'/timelapse/'.$day.'/'.$image.'" alt="">'.$image.'</a>';
 }
 
 ?>
