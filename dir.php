c91bd632301bb8d5a6cec45745aa92800202b469
Vegard Andersen <ferska.vara@gmail.com>
2015-12-30 12:34:42 +0100

    update dir.php

--- dir.php
+++ dir.php
@@ -3,10 +3,12 @@
 $day    = date("Y-m-d");
 //$dir    = '/var/www/html/timelapse/'.$day;
 $dir    = $_SERVER['DOCUMENT_ROOT'].'/timelapse/'.$day;
-$images = scandir($dir, 1);
-$images = array_slice($images,1);
+#$images = scandir($dir, 1);
+#$images = array_slice($images,1);
+$images = glob($dir .'/*.{jpeg,jpg}', GLOB_BRACE);
 
 foreach ($images as $image){
+	$image = basename($image);
 	echo '
 			<a id="webcam" class="fancybox hidden" rel="webcam" title="View from Joknuden" href="'.$REQUEST_URI.'/timelapse/'.$day.'/'.$image.'" alt="">'.$image.'</a>';
 }
