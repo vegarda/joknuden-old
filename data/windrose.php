9970e5988bd941ef4dc05e81ebf94f1496c2dbb8
Vegard Andersen <ferska.vara@gmail.com>
2016-01-04 22:59:08 +0100

    Greatly improve windrose performance

--- data/windrose.php
+++ data/windrose.php
@@ -1,6 +1,6 @@
 <?php
 
-$windrosestart = microtime(true);
+$scriptstart = microtime(true);
 
 include('startend.php');
 
@@ -8,67 +8,46 @@ if(is_integer($start) && (is_integer($end))){
     
 	include('config.php');
 	$joknuden = mysqli_connect($host, $user, $pass) or die(mysql_error()); 
-
-	$seconds = ($end - $start);
-	$count = $seconds/60;
-	$interval = 3*$count/$seconds;
-	header('Interval: '.$interval);
 	
 	$queryString = "SELECT dateTime, 
-
 	ROUND(MAX(windSpeed), 1) windSpeed, 
 	ROUND(MAX(windDir), 1) windDir
-	
 	FROM weewx.archive 
 	WHERE dateTime >= ".$start." and dateTime <= ".$end." 
-	GROUP BY ROUND(dateTime / ".$interval.")  
+	GROUP BY dateTime
 	ORDER BY dateTime ASC;";
 	
-	$query = mysqli_query($joknuden, $queryString);
+	header("X-Query: ".$queryString."");
 	
-    /*
-	$query = mysqli_query($joknuden, 
-    "SELECT windSpeed, windDir
-    FROM weewx.archive 
-    WHERE dateTime >= ".$start."
-    AND dateTime < ".$end."
-    ORDER BY dateTime ASC");
-	*/
-
-    $rows = Array();
-    while($row = mysqli_fetch_assoc($query)){
-      array_push($rows, $row);
-    }
-
-	header('Rows: '.count($rows));
+	$query = mysqli_query($joknuden, $queryString);
 	
     $windfreq = array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
     $windvelocity = array(0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0);
 
-    foreach ($rows as $row){
+	while($row = mysqli_fetch_assoc($query)){
         $ordinal = round($row['windDir'] / 22.5);
         $ordinal = $ordinal == 16 ? 0 : $ordinal;
 
         $windvelocity[$ordinal] += $row['windSpeed'];
         $windfreq[$ordinal] += 1;
     }
-    $windvector = $windvelocity;
+
+	$windvector = $windvelocity;
 
     $freqsum = array_sum($windfreq);
 
     foreach ($windfreq as $key => $value){
         if ($windfreq[$key] > 0){
-        $windvelocity[$key] = $windvelocity[$key]/$windfreq[$key];
+			$windvelocity[$key] = $windvelocity[$key]/$windfreq[$key];
         }
         else{
-        $windvelocity[$key] = 0;
+			$windvelocity[$key] = 0;
         }
-
         $windfreq[$key] = $windfreq[$key]*100/$freqsum;
         $windvector[$key] = $windvelocity[$key]*$windfreq[$key]/100;
     }
 
-	header('X-time: '.(microtime(true) - $windrosestart));
+	header('X-time: '.(microtime(true) - $scriptstart) * 1000);
 	
     echo json_encode([$windfreq, $windvelocity, $windvector]);
 }
